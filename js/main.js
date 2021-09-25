127c30f5645983853bfd28d909490b30af2a0f5a
BrendaZabala <brenda.zabala@outlook.com>
2021-09-24 21:32:32 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const boton = document.querySelector("#btn");
+    boton.addEventListener("click", function(evento){
+        alert("Validation is successful!");
+    });
+});
